6a5cccebb41a10d0f85d4a118cd6641c0de64392
gama2910 <134122632+AlexMor2910@users.noreply.github.com>
2026-07-25 23:35:23 -0600

    Update RouterComponent.jsx

--- src/components/RouterComponent.jsx
+++ src/components/RouterComponent.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import StartPage from "./StartPage.jsx";
 import ResultsPage from "./ResultsPage.jsx";
 import GamePage from "./GamePage.jsx";
@@ -6,7 +6,6 @@ import "./RouterComponent.css";
 
 export default function RouterComponent(props) {
     return (
-        <HashRouter>
             <div className="appShell">
                 <Routes>
                     <Route path="/" element={<StartPage excelFiles={props.excelFiles} setExcelFiles={props.setExcelFiles} excelNames={props.excelNames} setExcelNames={props.setExcelNames} excelLength={props.excelLength} setExcelLength={props.setExcelLength} buttonState={props.buttonState} setButtonState={props.setButtonState} inputValues={props.inputValues} setInputValues={props.setInputValues} questions={props.questions} setQuestions={props.setQuestions} />} />
@@ -15,6 +14,5 @@ export default function RouterComponent(props) {
                     <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
             </div>
-        </HashRouter>
     );
 }
