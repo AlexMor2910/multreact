ee3adbc4fe52a531a88ff43002106349532170c4
gama2910 <134122632+AlexMor2910@users.noreply.github.com>
2026-07-25 23:24:38 -0600

    Update RouterComponent.jsx

--- src/components/RouterComponent.jsx
+++ src/components/RouterComponent.jsx
@@ -1,4 +1,4 @@
-import {Navigate, Route, Routes} from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import StartPage from "./StartPage.jsx";
 import ResultsPage from "./ResultsPage.jsx";
 import GamePage from "./GamePage.jsx";
@@ -6,13 +6,15 @@ import "./RouterComponent.css";
 
 export default function RouterComponent(props) {
     return (
-        <div className="appShell">
-            <Routes>
-                <Route path="/" element={<StartPage excelFiles={props.excelFiles} setExcelFiles={props.setExcelFiles} excelNames={props.excelNames} setExcelNames={props.setExcelNames} excelLength={props.excelLength} setExcelLength={props.setExcelLength} buttonState={props.buttonState} setButtonState={props.setButtonState} inputValues={props.inputValues} setInputValues={props.setInputValues} questions={props.questions} setQuestions={props.setQuestions} />} />
-                <Route path="/game" element={<GamePage questions={props.questions} wrongQuestions={props.wrongQuestions} setWrongQuestions={props.setWrongQuestions} />} />
-                <Route path="/results" element={<ResultsPage wrongQuestions={props.wrongQuestions} />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-        </div>
+        <HashRouter>
+            <div className="appShell">
+                <Routes>
+                    <Route path="/" element={<StartPage excelFiles={props.excelFiles} setExcelFiles={props.setExcelFiles} excelNames={props.excelNames} setExcelNames={props.setExcelNames} excelLength={props.excelLength} setExcelLength={props.setExcelLength} buttonState={props.buttonState} setButtonState={props.setButtonState} inputValues={props.inputValues} setInputValues={props.setInputValues} questions={props.questions} setQuestions={props.setQuestions} />} />
+                    <Route path="/game" element={<GamePage questions={props.questions} wrongQuestions={props.wrongQuestions} setWrongQuestions={props.setWrongQuestions} />} />
+                    <Route path="/results" element={<ResultsPage wrongQuestions={props.wrongQuestions} />} />
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+            </div>
+        </HashRouter>
     );
 }
